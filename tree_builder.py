a93ee2e2f015a849b654a575d28e70bf3c6704fc
Amit <amitshechnik@gmail.com>
2024-12-14 21:11:36 +0200

    change expression type from str to list

--- tree_builder.py
+++ tree_builder.py
@@ -2,12 +2,12 @@ from Tree import *
 from calculation import *
 from exceptions import *
 
-OP_POWER = {'-': 1, '+': 1, '*': 2, '/': 2, '^': 3, '@': 5, '$': 5, '&': 5, '%': 4, '~': 6, '!': 6}
+OP_POWER = {'-': 1, '+': 1, '*': 2, '/': 2, ';': 2.5, '^': 3, '@': 5, '$': 5, '&': 5, '%': 4, '~': 6, '!': 6, '#':6}
 
-def is_closed(expression):
-    #the func check if th exp start and end with the same parentheses
+def is_closed(expression:list) -> bool:
+    """the func check if th exp start and end with the same parentheses"""
     count=0
-    for char in expression[1:-1]:
+    for char in expression[1:len(expression)-1]:
         if char == '(':
             count += 1
         elif char == ')':
@@ -17,13 +17,14 @@ def is_closed(expression):
     return count == 0
 
 
-def find_min(expression):
-    #The function finds the operator with the lowest precedence
+def find_min(expression: list) -> int:
+    """The function finds the operator with the lowest precedence"""
     min_op = '0'
     min_index = -1
     count = 0
 
-    for i, x in enumerate(expression):
+    for i in range(len(expression)):
+        x=expression[i]
         if x == "(":
             count += 1
         elif x == ")":
@@ -36,57 +37,64 @@ def find_min(expression):
     return min_index
 
 
-def build_tree(expression: str, root: TreeNode) -> TreeNode:
-    #the func build the tree that represent the expression
+def build_tree(expression: list, root: TreeNode) -> TreeNode:
+    """the func build the tree that represent the expression"""
+    if not expression:
+        root.set_value('0')
+        return root
+
+    if len(expression) == 1:
+        root.set_value(expression[0])
+        return root
 
     #remove parentheses if needed
-    if expression[0] == '(' and expression[-1] == ')':
+    if len(expression) >= 2 and expression[0] == '(' and expression[-1] == ')':
         if is_closed(expression):
             expression = expression[1:-1]
+
+    #raise an error if parentheses aer empty
+    if not expression:
+        raise IncorrectSyntax("Syntax Error: parentheses '()' are empty")
 
     #split the expression by the weakest operand.
     min_op = find_min(expression)
 
-    if min_op == -1:
-        root.set_value(expression)
-        return root
-    else:
-        root.set_value(expression[min_op])
-        first_part, second_part = expression[:min_op], expression[(min_op + 1):]
+    root.set_value(expression[min_op])
+    first_part, second_part = expression[:min_op], expression[(min_op + 1):]
 
-        if first_part == '':
-            first_part = '0'
-        if second_part == '':
-            second_part = '0'
-        build_tree(first_part, root.get_left())
-        build_tree(second_part, root.get_right())
-        return root
+    build_tree(first_part, root.get_left())
+    build_tree(second_part, root.get_right())
+    return root
 
 def calculate_tree(root: TreeNode)-> float:
-    #caculate the tree expression
+    """calculate the tree expression"""
+
+    #stopping condition
     if root.is_leaf():
         return root.get_value()
     else:
         op1=calculate_tree(root.get_left())
         op2=calculate_tree(root.get_right())
-        try:
-            cal= Operation[root.get_value()](op1, op2)
-            result= cal.result()
-        except Exception as e:
-            print(e.message)
-        else:
-            return result
+
+        cal= Operation[root.get_value()](root.get_value(),op1, op2)
+        result= cal.result()
+
+        return result
 
 
 def main():
-    expression = "((3+2)-(6*8))"
+    expression =  ['4', '+', '.','*', '2']
 
-    root = TreeNode()
-    root = build_tree(expression, root)
-    result= calculate_tree(root)
-    print(f"the result is: {result}")
-    # root.print_infix()
-    # print("--------------")
-    # root.print_prefix()
+    try:
+        root = TreeNode()
+        root = build_tree(expression, root)
+        result= calculate_tree(root)
+    except Exception as e:
+        print(e.message)
+    else:
+        print(f"the result is: {result}")
 
-main()
+
+if __name__ == "__main__":
+    main()
+
